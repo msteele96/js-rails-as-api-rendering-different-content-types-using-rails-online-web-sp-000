75360eb07f9087bbad325882df05f126df8ce85a
msteele96 <marcsteele2014@gmail.com>
2021-04-29 11:47:49 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019) do
+ActiveRecord::Schema.define(version: 2021_04_29_153811) do
 
   create_table "birds", force: :cascade do |t|
     t.string "name"
